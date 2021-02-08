489d647518cd65b805f8bb4520ac19e7508cf92d
Jean Kang <jeankang.dev@gmail.com>
2021-02-08 02:35:59 -0500

    Express server setup

--- 1__windowSize.js
+++
@@ -1,6 +0,0 @@
-// https://velog.io/@yunaineworld
-
-// window.screen => monitor size
-// window.outer  => browser size 
-// window.inner  => webpage including scrolls
-// documentElement.clientWidth => webpage excluding scrolls
---
+++ index.js
@@ -0,0 +1,9 @@
+// https://velog.io/@yunaineworld
+
+// window.screen => monitor size
+// window.outer  => browser size 
+// window.inner  => webpage including scrolls, URL, tab
+// documentElement.clientWidth => webpage excluding scrolls
+
+let windowScreen = window.screen;
+console.log(windowScreen);
