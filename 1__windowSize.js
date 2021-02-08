5dce333e860f1f448749e13d1c478d172ea616ce
Jean Kang <jeankang.dev@gmail.com>
2021-02-07 23:54:45 -0500

    1__windowSize

--- 1__windowSize.js
+++ 1__windowSize.js
@@ -1,1 +1,6 @@
 // https://velog.io/@yunaineworld
+
+// window.screen => monitor size
+// window.outer  => browser size 
+// window.inner  => webpage including scrolls
+// documentElement.clientWidth => webpage excluding scrolls
