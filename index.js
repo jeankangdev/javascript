4161217794dc4ba83b50556dce4dd8df28fe2398
Jean Kang <jeankang.dev@gmail.com>
2021-02-08 07:31:17 -0500

    render HTML on node js

--- index.js
+++
@@ -1,1 +0,0 @@
-// https://velog.io/@yunaineworld
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
-// app.set('view engine', 'html');
-
-// app.use('/', function (req, res) {
-//   res.render('./index');
-// })
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
