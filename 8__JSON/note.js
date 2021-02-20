af18d82e648797ecb8970de239b27c569bbf310f
Jean Kang <jeankang.dev@gmail.com>
2021-02-20 03:30:24 -0500

    add synchronous & asynchronous

--- 8__JSON/note.js
+++
@@ -1,9 +0,0 @@
-// JSON
-// JavaScript Object Notation
-
-// 1. Object to JSON
-let json = JSON.stringify(true);
-console.log(json);
-
-// 2. JSON to Object
-// parse(json)
--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,77 @@
 'use strict';
 
-// JSON
-// JavaScript Object Notation
+// JavaScript is synchronous
+// Execute the code block 'by order' after hoisting
+// hoisting: var, function declaration
 
-// 1. Object to JSON
+// 1. synchronous VS asynchronous
 
-const rabbit = {
-  name: 'tori',
-  size: null,
-  birthDate: new Date(),
-  // symbol: Symbol("id"),
-  jump: () => {
-    console.log(`${name} can jump!`);
+// synchronous: by order
+// asynchronous: not by order (setTimeout)
+
+// 2. synchronous callback VS asynchronous callback
+
+// synchronous callback
+function printImmediately(print) {
+  print();
+}
+printImmediately(() => console.log('sync callback'));
+
+// asynchronous callback
+function printWithDelay(print, timeout) {
+  setTimeout(print, timeout);
+}
+printWithDelay(() => console.log('async callback'), 2000);
+
+// 3. callback hell experience
+
+class UserStorage {
+  loginUser(id, password, onSuccess, onError) {
+    setTimeout(() => {
+      if (
+        (id === 'ellie' && password === 'dream') ||
+        (id === 'coder' && password === 'academy')
+      ) {
+        onSuccess(id);
+      } else {
+        onError(new Error('not found'));
+      }  
+    }, 2000);
   }
-};
 
-// option 1
-// let json = JSON.stringify(rabbit);
+  getRoles(user, onSuccess, onError) {
+    setTimeout(() => {
+      if (user === 'ellie') {
+        onSuccess({ name: 'ellie', role : 'admin' });
+      } else {
+        onError(new Error('no access'));
+      }  
+    }, 1000);
+  }
 
-// option 2 (with callback)
-let json = JSON.stringify(rabbit, (key, value) => {
-  return key === 'name'? 'yuna' : value;
-});
+  onSuccess(id) {
+    return id;
+  }
+}
 
-console.log(json);
+const id = prompt('enter your id');
+const pw = prompt('enter your pw');
 
-// 2. JSON to Object
-// option 1
-// const obj = JSON.parse(json);
-
-// option 2 (with callback)
-const obj = JSON.parse(json, (key, value) => {
-  return key === 'birthDate'? new Date(value): value; // change JSON(string) data to obj
-});
-
-console.log(obj);
-
-// rabbit.jump(); // original data had the jump function
-// obj.jump();    // error: JSON can't have a function as a data, so the obj (that is parced from JSON) can't have the function as well
-
-// console.log(rabbit.birthDate.getDate()); // original data had the birthDate method
-// console.log(obj.birthDate.getDate());    // error: birthDate is string (original data -> JSON(string) -> obj)
-
+const userStorage = new UserStorage();
+userStorage.loginUser(
+  id, 
+  pw, 
+  (user) => {
+    userStorage.getRoles(
+      user, 
+      (userWithRole) => {
+        alert(`Hi ${userWithRole.name}, you have a ${userWithRole.role} role.`);
+      },
+      error => {
+        console.log(error);
+      });
+  }, 
+  (error) => {
+    console.log(error);
+  }
+);
