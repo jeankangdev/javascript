22cc0ec7cc0fe8eca52de0c6ab7be7c176b272ca
Jean Kang <jeankang.dev@gmail.com>
2021-02-21 23:56:28 -0500

    add note of optional chaining

--- 0__modern-JavaScript/object/4-6__optional-chaining.js
+++ 0__modern-JavaScript/object/4-6__optional-chaining.js
@@ -3,4 +3,19 @@
 
 'use strict';
 
-// 1. 
+// ex 1.
+// let user = {};
+// console.log( user && user.address );  // no error even if user or user.address is undefined
+// console.log( user?.address );        // no error
+
+// ex 2.
+let user1 = {
+  admin() {
+    console.log("admin");
+  }
+}
+
+let user2 = {};
+
+user1.admin?.();  // "admin"
+user2.admin?.();  // stops with no error even if user2 has no methods admin
