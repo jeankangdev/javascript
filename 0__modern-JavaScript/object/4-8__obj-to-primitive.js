8c0c53cd39c21f20b09a318a3ea33e955b74ad54
Jean Kang <jeankang.dev@gmail.com>
2021-02-23 11:01:25 -0500

    add note of obj-to-primitive

--- 0__modern-JavaScript/object/4-8__obj-to-primitive.js
+++ 0__modern-JavaScript/object/4-8__obj-to-primitive.js
@@ -1,5 +1,24 @@
 // convert object to primitive type
 // https://ko.javascript.info/object-toprimitive
 
+// obj -> primitive: when calculate / print obj
+// 1, str ( alert(obj) )
+// 2. num (+obj)
+// 3. default ( obj + obj, obj + ~, obj == ~)
+
+
 'use strict';
 
+let user = {
+  name: "John",
+  money: 1000,
+
+  [Symbol.toPrimitive](hint) {
+    alert(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  }
+};
+
+alert(user);	// hint: string // {name: "John"}
+alert(+user);	// hint: number // 1000
+alert(user + 500);	// hint: default // 1500
