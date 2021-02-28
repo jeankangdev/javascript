60f1989cc7ec568299165fcba1cb6de64807fd0f
Jean Kang <jeankang.dev@gmail.com>
2021-02-27 23:52:27 -0500

    add note

--- 0__modern-JavaScript/object/5-2__number-type.js
+++ 0__modern-JavaScript/object/5-2__number-type.js
@@ -3,6 +3,8 @@
 
 'use strict';
 
+// 1.
+
 let hundred = 1e2;        // 100
 console.log(hundred);
 
@@ -13,4 +15,11 @@ console.log(minusHundred);
 let binary = 0b11111111;      // 255
 let octal = 0o377;            // 255
 let hexa = 0xff;              // 255
+
+// 2. .toString(base): convert from original to base and print it (with string type)
+
+let num = 255;
+console.log(num.toString(10));  // 255
+console.log(num.toString(2));   // 11111111
+console.log(num.toString(16));  // ff
 
