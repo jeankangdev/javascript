ba1f8972bf988b52a8225e4d36fc0b80001dfe88
Jean Kang <jeankang.dev@gmail.com>
2021-03-02 07:14:00 -0500

    add note

--- 0__modern-JavaScript/object/5-4__array.js
+++ 0__modern-JavaScript/object/5-4__array.js
@@ -4,6 +4,7 @@
 'use strict';
 
 // array is a special type of obj, it has an order unlike obj
+// obj (includes array) is copied by reference
 
 // how to declare an array?
 let array = [item1, item2...];
@@ -70,3 +71,87 @@ console.log(+obj);    // 16
 alert(obj);           // "yuna"
 console.log(obj + 10);// 26
 
+// Q. create an array which has "Jazz", "Blues" and change the array as shown below
+Jazz, Blues
+Jazz, Blues, Rock-n-Roll
+Jazz, Classics, Rock-n-Roll
+Classics, Rock-n-Roll
+Rap, Reggae, Classics, Rock-n-Roll
+
+// A
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+
+let centerIndex = Math.round((styles.length / 2) - 1);
+styles[centerIndex] = "Classics";
+
+console.log(styles.shift());
+styles.unshift("Rap", "Reggae");
+console.log(styles);
+
+// Q. expect the result of the code below
+
+let arr = ["a", "b"];
+
+arr.push(function() {
+  alert( this );
+})
+
+arr[2]();
+
+// A. 
+arr = ["a", "b", function() { alert( this ); }];
+arr[2]() => function() { alert( this ); }];
+this => arr
+alert => the obj's hint: str => array doesn't have Symbol.toPrimitive or valueOf() method for default/number type => the array is converted to str
+// the alert window prints str as shown below
+a,b,function() {
+  alert( this );
+}
+
+// Q. write the function sumInput() that:
+// asks the user for values using prompt and stores the values in the array
+// finishes asking when the user enters a non-numeric value, an empty string, or presses "Cancel"
+// calculates and returns the sum of array items
+// P.S. A zero(0) is a valid number, please don't stop the input on zero
+
+// A.
+function sumInput() {
+  let array = [];
+  let answer;
+  let sum = 0;
+
+  while (true) {
+    answer = prompt("please input a number");
+    if (!isFinite(answer) || answer === '' || answer === null) {
+      break;
+    }
+    array.push(answer);
+  }
+  for (let item of array) {
+    sum += +item;
+  }
+  return sum;
+}
+
+alert(sumInput());
+
+// Q. write the function getMaxSubSum(arr) that finds the contiguous subarray of arr with the maximal sum of items
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let partialSum = 0;
+
+  for (let item of arr) {
+    partialSum += item;
+    maxSum = Math.max(maxSum, partialSum);
+    if (partialSum < 0) partialSum = 0;
+  }
+  return maxSum;
+}
+
+alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
+alert( getMaxSubSum([1, 2, 3]) ); // 6
+alert( getMaxSubSum([-1, -2, -3]) ); // 0
--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
 'use strict';
 
-console.log([] + 1 );      // "1"
-console.log( [1] + 1);      // "11"
-console.log( [1, 2] + 1);   // "1,21"
+function getMaxSubSum(arr) {
+  let maxSum = 0;
+  let partialSum = 0;
+
+  for (let item of arr) {
+    partialSum += item;
+    maxSum = Math.max(maxSum, partialSum);
+    if (partialSum < 0) partialSum = 0;
+  }
+  return maxSum;
+}
+
+alert( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+alert( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+alert( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
+alert( getMaxSubSum([1, 2, 3]) ); // 6
+alert( getMaxSubSum([-1, -2, -3]) ); // 0
