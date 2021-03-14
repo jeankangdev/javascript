15d4b6b9af7708247fee53a7b27bd070dcecdd6c
Jean Kang <jeankang.dev@gmail.com>
2021-03-14 13:32:38 -0400

    2__minNum

---
+++ 00__leet/2__minNum.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function minNum(a, b, c) {
+  let minNum;
+  if (a < b) minNum = a;
+  else minNum = b;
+  if (c < minNum) minNum = c;
+  
+  return minNum;
+}
+
+console.log(minNum(6, 5, 11));
