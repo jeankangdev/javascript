b3d685f41fd3921e390c4be571553320bf36b3fe
Jean Kang <jeankang.dev@gmail.com>
2021-03-16 08:21:29 -0400

    11__how many chars

--- 00__leet/10__A-to-#.js
+++ 00__leet/10__A-to-#.js
@@ -1,11 +1,17 @@
 'use strict';
 
 function solution(str) {
+  // 1. my way
 	let answer = [];
 	str.split('').map((char) => {
 		char === 'A'? answer.push('#') : answer.push(char);
 	});
 	return answer.join('');
+
+  // 2. using array.replace()
+  let answer = str; // str is copied by value, not copied by reference
+  answer = answer.replace(/A/g, '#');
+  return answer;
 }
 
 let str = 'BANANA';
---
+++ 00__leet/11__how-many-chars.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function solution(str) {
+  let char = prompt('type a letter');
+  let counter = 0;
+
+  // 1.
+  str.split('').forEach((elem) => {
+    if (elem === char) {
+		counter++;
+    }
+
+  // 2.
+  counter = str.split(char) - 1;
+  });
+
+  return counter;
+}
+
+let str = 'COMPUTERPROGRAMMING';
+console.log(solution(str));
