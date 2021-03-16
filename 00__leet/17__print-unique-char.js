adbcbef0c09042eb8fd89ef99958b436a1e0e53f
Jean Kang <jeankang.dev@gmail.com>
2021-03-16 17:54:07 -0400

    17__print unique char

--- 00__leet/17__print-unique-char.js
+++ 00__leet/17__print-unique-char.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function solution(str) {
+  // 1. my way 
 	let answer = [];
 
 	str.split('').map((char) => {
@@ -8,6 +9,14 @@ function solution(str) {
 	});
 	
 	return answer.join('');
+
+  // 2. use str.indexOf(char)
+  let answer = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str.indexOf(str[i]) === i) answer += str[i];
+  }
+
+  return answer;
 }
 
 let str = 'ksekkset';
