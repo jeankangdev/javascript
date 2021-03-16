e8dee25d3a36597c8c27c616a7786bcaeb6f2df7
Jean Kang <jeankang.dev@gmail.com>
2021-03-16 08:55:46 -0400

    12__find capital

--- 00__leet/12__find-capital.js
+++ 00__leet/12__find-capital.js
@@ -1,11 +1,21 @@
 'use strict';
 
 function solution(str) {
+  // 1. my way
 	let counter = 0;
 
 	str.split('').forEach((char) => {
 		if (char === char.toUpperCase()) counter++;
 	});
+
+  // 2. use ASCII
+
+  for(let char of str) {
+    let asciiNum = char.charCodeAt();
+    if (asciiNum >= 65 && asciiNum <= 90) { // Capital: 65 ~ 90 // LowerCase: 97 ~ 122
+      counter++;
+    }
+  }
 
 	return counter;
 }
