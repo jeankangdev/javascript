b834b56333f39ae711a7b5a722374bcd10ef475e
Jean Kang <jeankang.dev@gmail.com>
2021-03-21 04:33:19 -0400

    27__palindrome with validation check

--- 00__algorithm/3__string/27__palindrome-with-validation-check.js
+++ 00__algorithm/3__string/27__palindrome-with-validation-check.js
@@ -7,21 +7,21 @@ function solution(str) {
 	// let answer = "YES";
 
   // 1. my way: array.filter() needs the keyword 'return'
-	// let filteredStr = str.toLowerCase().split('').filter((char) => {
-	// 	ASCII
-	// 	UpperCase: 65 ~ 90 (90 - 65 = 25)
-	// 	LowerCase: 97 ~ 122 (122 - 97 = 25)
-	// 	return (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90 || char.charCodeAt(0) >= 97 && char.charCodeAt(0) < 122);
-	// }).join('');
+	let filteredStr = str.toLowerCase().split('').filter((char) => {
+		// ASCII
+		// UpperCase: 65 ~ 90 (90 - 65 = 25)
+		// LowerCase: 97 ~ 122 (122 - 97 = 25)
+		return (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90 || char.charCodeAt(0) >= 97 && char.charCodeAt(0) < 122);
+	}).join('');
   
 	// now filteredStr is lower case, with only alphabets
-  // console.log(filteredStr);
+  console.log(filteredStr);
   
-	// let n = filteredStr.length;	
-	// for (let i = 0; i < n / 2; i++) {
-  //   if (filteredStr[i] !== filteredStr[n-i-1]) return "NO";
-  // }
-  // return answer;
+	let n = filteredStr.length;	
+	for (let i = 0; i < n / 2; i++) {
+    if (filteredStr[i] !== filteredStr[n-i-1]) return "NO";
+  }
+  return answer;
 
   // 2. simpler way: use string.replace(condition, to)
   // str.toLowerCase() changes the original str
