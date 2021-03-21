4be180ef66ac279e92a759703f63e037ad899cfd
Jean Kang <jeankang.dev@gmail.com>
2021-03-21 01:25:24 -0400

    25__count mountain tops from a map

--- 00__algorithm/2__array/25__count-mountain-tops.js
+++ 00__algorithm/2__array/25__count-mountain-tops.js
@@ -7,14 +7,12 @@
 // how many mountain tops does this map have?
 
 function solution(map) {
-
-  // 1. my way: add 4 corners(0)
 	let countMountains = 0;
 	let n = map.length;
-  
+
   // add 0 for 4 corners (makes the map 7 * 7)
 	for (let i = 0; i < n; i++) {
-    map[i].unshift(0);
+		map[i].unshift(0);
 		map[i].push(0);
 	}
   
@@ -26,35 +24,10 @@ function solution(map) {
   // diagnose which region(cell) is the mountain top (from 5 * 5 map)
 	for (let i = 1; i <= n; i++) {
 		for (let j = 1; j <= n; j++) {
-			if (map[i][j] > map[i-1][j] && map[i][j] > map[i][j-1] && map[i][j] > map[i][j+1] && map[i][j] > map[i+1][j]) countMountains++;
+			if (map[i][j] > map[i-1][j] && map[i][j] > map[i][j-1] && map[i][j] > map[i][j+1] && map[i+1][j]) countMountains++;
 		}
 	}
-
-	return countMountains;  // 10
-
-  // 2. simpler way: use array for x / y location
-  let countMountains = 0;
-  let n = map.length;
-
-  let moveRow = [-1, 0, 1, 0];
-  let moveCell = [0, 1, 0, -1];
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-
-      let isMountainTop = true;
-
-      for (let k = 0; k < moveRow.length; k++) {
-        let newRow = i + moveRow[k];
-        let newCell = j + moveCell[k];
-        
-        if (newRow >= 0 && newRow < n && newCell >= 0 && newCell < n && map[i][j] <= map[newRow][newCell]) isMountainTop = false;
-      }
-
-      if (isMountainTop === true) countMountains++;
-    }
-  }
-  return countMountains;  // 10
+	return countMountains;
 }
 
 let map = [
