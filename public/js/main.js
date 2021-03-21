a198fb7c08478da8db9d6be3fa6016ad29ff2890
Jean Kang <jeankang.dev@gmail.com>
2021-03-21 05:07:14 -0400

    28__extract numbers

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,17 @@
 'use strict';
 
-// palindrome with validation check (for alphabets)
-// not case sensitive
-
 function solution(str) {
-	let answer = "YES";
+  // str.replace(condition, to) doesn't work for numbers
+	// return str.replace(/[^0-9]/, '');
 
-  // array.filter() needs the keyword 'return'
-	let filteredStr = str.toLowerCase().split('').filter((char) => {
-		// ASCII
-		// UpperCase: 65 ~ 90 (90 - 65 = 25)
-		// LowerCase: 97 ~ 122 (122 - 97 = 25)
-		return (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90 || char.charCodeAt(0) >= 97 && char.charCodeAt(0) < 122);
-	}).join('');
+  let answerStr = str.split('').filter((char) => {
+    return isNaN(+char) === false
+  }).join('');  // '0208'
 
-	// now filteredStr is lower case, with only alphabets
-  console.log(filteredStr);
-  
-  // 1. use for loop
-	// let n = filteredStr.length;	
-	// for (let i = 0; i < n / 2; i++) {
-  //   	if (filteredStr[i] !== filteredStr[n-i-1]) return "NO";
-  //   }
-  // return answer;
+  let answerNum = +answerStr; // 208
 
-  // 2. use array.reverse()
-  return (filteredStr === filteredStr.split('').reverse().join(''))? "YES" : "NO";
+  return answerNum;
 }
 
-let str = "Go2o?og";	// 'gooog'
-console.log(solution(str)); // 'YES'
+let str = 'g0en2T0s8eSoft';
+console.log(solution(str)); // 208
