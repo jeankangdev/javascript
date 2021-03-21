bc9161644311c6d89ae514b13531f6581d66e08f
Jean Kang <jeankang.dev@gmail.com>
2021-03-20 22:44:22 -0400

    24__maxSum of a table

--- 00__leet/23__ranks.js
+++ 00__leet/23__ranks.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function solution(marks) {
+  // 1. my way
 	let ranks = [1, 1, 1, 1, 1];
 
 	for (let i = 0; i < marks.length; i++) {
@@ -10,6 +11,11 @@ function solution(marks) {
 	}
 
 	return ranks;
+
+  // 2. use Array.from()
+  let ranks = Array.from({length: marks.length}, () => 1);
+  // now ranks = [1, 1, 1, 1, 1];
+  
 }
 
 let marks = [87, 89, 92, 100, 76];
