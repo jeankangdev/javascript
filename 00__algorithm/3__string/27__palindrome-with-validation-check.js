4e40f828c09eca6bd7c51dba83ef84ddbc39a2ac
Jean Kang <jeankang.dev@gmail.com>
2021-03-21 04:17:54 -0400

    27__palindrome with validation check

--- 00__algorithm/3__string/27__palindrome-with-validation-check.js
+++ 00__algorithm/3__string/27__palindrome-with-validation-check.js
@@ -6,21 +6,26 @@
 function solution(str) {
 	let answer = "YES";
 
+  // array.filter() needs the keyword 'return'
 	let filteredStr = str.toLowerCase().split('').filter((char) => {
 		// ASCII
 		// UpperCase: 65 ~ 90 (90 - 65 = 25)
 		// LowerCase: 97 ~ 122 (122 - 97 = 25)
 		return (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90 || char.charCodeAt(0) >= 97 && char.charCodeAt(0) < 122);
-	});
+	}).join('');
 
 	// now filteredStr is lower case, with only alphabets
-	filteredStr = filteredStr.join(''); // array.join('') doesn't affect the original array 
+  console.log(filteredStr);
+  
+  // 1. use for loop
+	// let n = filteredStr.length;	
+	// for (let i = 0; i < n / 2; i++) {
+  //   	if (filteredStr[i] !== filteredStr[n-i-1]) return "NO";
+  //   }
+  // return answer;
 
-	let n = filteredStr.length;	
-	for (let i = 0; i < n / 2; i++) {
-		if (filteredStr[i] !== filteredStr[n-i-1]) return "NO";
-	}
-	return answer;
+  // 2. use array.reverse()
+  return (filteredStr === filteredStr.split('').reverse().join(''))? "YES" : "NO";
 }
 
 let str = "Go2o?og";	// 'gooog'
